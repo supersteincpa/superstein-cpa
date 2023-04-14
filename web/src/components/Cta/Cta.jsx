2e73674c20d505fb394ea6628700ff776562cbf6
aqilusman45 <sunnyrajputtrana00@gmail.com>
2023-04-13 20:57:13 -0700

    fix: cta fixes

--- web/src/components/Cta/Cta.jsx
+++ web/src/components/Cta/Cta.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
 import clsx from 'clsx'
-import { graphql } from 'gatsby'
+
+
 import './cta.scss'
+import Button from '../../components/Button/Button'
+import Heading from '../Heading/Heading'
 
 export const Cta = ({
   otherClasses,
-  mainHeading,
-  _rawSubText,
-  button,
-  toggleFunc,
+
 }) => {
   const ctaClasses = clsx(
     otherClasses,
-    'w-full h-[572px] lg:h-[236px] relative pt-10'
+    'w-full cta-clip-path bg-gray-50 py-[91px] lg:py-[210px] '
   )
 
-  return <section className={ctaClasses} data-testid="cta"></section>
+  return <section className={ctaClasses} data-testid="cta">
+    <div className='w-full max-w-[1512px]  mx-auto flex flex-col lg:flex-row items-start lg:items-center px-6 lg:px-20 justify-center gap-6 relative'>
+      <p className='absolute top-[50%] left-0 text-sm font-bold text-gray-300 transition sm:hidden -translate-y-[-50%] -rotate-90 hidden lg:block'>
+        08. contact us
+      </p>
+      <Heading type='h2' otherClasses='w-[200px] lg:w-[185px] lg:min-w-[185px] text-[28px] lg:text-4xl font-normal leading-[42px]'>Let’s Work Together</Heading>
+      <div className='border boder-[1px] border-[#4679E6] w-full h-0 lg:w-0 lg:h-[114px] float-left'></div>
+      <div className='lg:max-w-[308px]'>
+        <p className='text-sm lg:text-base font-normal leading-6 text-typography tracking-[0.03em] w-full'>
+          If you're dealing with intricate and complex finances, you need Superstein PA on your side. Reach out to us today.
+        </p>
+      </div>
+      <div className='flex gap-6'>
+        <Button variant='secondary' label='Contact us' otherClasses="whitespace-nowrap" />
+        <Button variant='primary' label='Book a call' otherClasses="whitespace-nowrap" />
+      </div>
+    </div>
+  </section>
 }
 
 export default Cta
--- web/src/components/Footer/Footer.jsx
+++ web/src/components/Footer/Footer.jsx
@@ -50,7 +50,7 @@ export const Footer = ({ otherClasses }) => {
       </div>
       <div className='flex flex-col mt-6 gap-[26px] lg:gap-0 lg:flex-row items-start lg:justify-between'>
         <ul className='flex flex-col space-y-4 lg:space-y-0 lg:flex-row items-start lg:items-center justify-start lg:space-x-4'>
-          <li className=' font-Public_Sans font-normal text-sm leading-[20px] tracking-[0.03em] text-white opacity-70'>©2023 Superstein PA</li>
+          <li className=' font-Public_Sans font-normal text-sm leading-[20px] tracking-[0.03em] text-white opacity-70'>&copy;{Year} Superstein PA</li>
           <li className=' font-Public_Sans font-normal text-sm leading-[20px] tracking-[0.03em] text-white opacity-70'>Privacy Policy</li>
           <li className=' font-Public_Sans font-normal text-sm leading-[20px] tracking-[0.03em] text-white opacity-70'>Accessibility Statement</li>
           {/* <li className=' font-Public_Sans font-normal text-sm leading-[20px] tracking-[0.03em] text-white opacity-70 flex gap-2 items-center justify-center'>
