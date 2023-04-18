dc80eb93d2eead1deaef5bbee21c3e933797ec8c
aqilusman45 <aqilusman45@gmail.com>
2023-04-18 09:55:23 +0500

    feat: add industries slider

--- web/src/sections/IndustriesSliderSection/IndustriesSliderSection.jsx
+++ web/src/sections/IndustriesSliderSection/IndustriesSliderSection.jsx
@@ -5,6 +5,7 @@ import Heading from '../../components/Heading/Heading'
 import Slider from '../../components/Slider/Slider'
 import ServicesCard from '../../components/ServicesCard/ServicesCard'
 import './industriesslidersection.scss'
+import Icon from '../../components/Icon/Icon'
 
 export const IndustriesSliderSection = ({
   otherClasses,
@@ -13,9 +14,9 @@ export const IndustriesSliderSection = ({
 }) => {
   const industriesSliderSectionClasses = clsx(
     otherClasses,
-    'max-w-[1512px] px-6 lg:pl-20 xl:pl-[156px] relative lg:my-[120px]'
+    'max-w-[1512px] relative mb-[140px] mt-[64px] lg:my-[120px]'
   )
-
+  // px-6 lg:pr-0 lg:pl-20 xl:pl-[156px]
   const industries = useStaticQuery(graphql`
     query IndustrySliderData {
       allSanityIndustryPages {
@@ -36,13 +37,84 @@ export const IndustriesSliderSection = ({
   `)
   const nodes = industries.allSanityIndustryPages.nodes
 
+  const location = typeof window !== 'undefined' && window.location.pathname
+  const currentLocation =
+    location &&
+    location.substring(0, location.length - 1).slice(1, location.length - 1)
+  console.log(currentLocation)
+
+  const filterBypathName = nodes.filter(
+    (nodes) => nodes.slug.current != currentLocation
+  )
+
+  const TestimonailPrevArrow = (props) => {
+    const { onClick } = props
+    return (
+      <button
+        onClick={onClick}
+        className="testimonail_arrow_box_shadow p-2 h-fit rounded-full bg-addition_button_color absolute bottom-[-62px] left-4 lg:left-20 xl:left-[120px] z-10"
+      >
+        <Icon
+          iconWidth={18}
+          iconHeight={18}
+          icon="testimonail-left-blue-icon"
+        />
+      </button>
+    )
+  }
+
+  const TestimonailNextArrow = (props) => {
+    const { onClick } = props
+    return (
+      <button
+        onClick={onClick}
+        className="testimonail_arrow_box_shadow p-2 h-fit rounded-full bg-addition_button_color absolute bottom-[-62px] right-4 lg:right-20 xl:right-[120px] rotate-180 z-10"
+      >
+        <Icon
+          iconWidth={18}
+          iconHeight={18}
+          icon="testimonail-left-blue-icon"
+        />
+      </button>
+    )
+  }
+
   const settings = {
     infinite: true,
     speed: 1000,
+    dots: true,
     centerMode: true,
     centerPadding: '40px',
     slidesToShow: 4,
     slidesToScroll: 1,
+    nextArrow: <TestimonailNextArrow />,
+    prevArrow: <TestimonailPrevArrow />,
+
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          centerMode: false,
+          centerPadding: '0px',
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   }
 
   return (
@@ -57,12 +129,18 @@ export const IndustriesSliderSection = ({
           </p>
         </div>
       )}
-      <Heading type="h2" otherClasses="text-gray-900 font-normal font-Poppins">
+      <Heading
+        type="h2"
+        otherClasses="lg:block hidden text-gray-900 font-normal font-Poppins px-6 lg:px-20 xl:px-[156px]"
+      >
         {mainHeading}
       </Heading>
-      <div className="mt-10">
-        <Slider customSettings={settings} customClass="industries_slider_container">
-          {nodes.map((nodes, index) => {
+      <div className="lg:mt-10 industries_slider_container overflow-x-clip w-full relative after:hidden md:after:block after:absolute after:right-0 after:w-[472px] after:h-full after:top-0">
+        <Slider
+          customSettings={settings}
+          customClass="industries_inner_slider px-6 lg:pr-0 lg:pl-20 xl:pl-[156px]"
+        >
+          {filterBypathName.map((nodes, index) => {
             return (
               <div className="px-[11px]">
                 <ServicesCard
