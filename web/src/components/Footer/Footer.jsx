4876134e85391bf875e33048bd7f200d155477eb
Zac Wood <57919069+Zac-Wood@users.noreply.github.com>
2023-09-07 15:17:35 -0700

    Update Footer.jsx
    https://app.clickup.com/t/8669pfmgd

--- web/src/components/Footer/Footer.jsx
+++ web/src/components/Footer/Footer.jsx
@@ -117,13 +117,13 @@ export const Footer = ({ otherClasses, services, industries, locations }) => {
               3830 Hollywood Blvd. <br /> Suite 101 <br /> Hollywood FL 33021{' '}
               <br /> (954) 602-9100
             </li>
-            {/* <li className="text-white font-semibold text-base leading-6 mb-2">
+            <li className="text-white font-semibold text-base leading-6 mb-2">
               Greenville Office
             </li>
             <li className=" font-Public_Sans font-normal text-sm leading-[20px] tracking-[0.03em] text-white opacity-70">
               355 South Main Street <br /> 1st & 2nd Floors <br /> Greenville, SC 29601 <br/>(123) 456-7890
             </li>
-            <br /> */}
+            <br /> 
             <li className="text-white font-semibold text-base leading-6 mb-2">
               Miami Office
             </li>
